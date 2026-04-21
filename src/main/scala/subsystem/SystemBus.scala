f17e69c550457d947318353a5e2fb9297dde0058
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2026-04-20 20:14:17 -0500

    Updates for mem traffic controller infra

--- src/main/scala/subsystem/SystemBus.scala
+++ src/main/scala/subsystem/SystemBus.scala
@@ -46,7 +46,9 @@ class SystemBus(params: SystemBusParams, name: String = "system_bus")(implicit p
 
 
   val memTrafficControl = p(MTCKey) match {
+    case Some(params: PerBankDRAMParams) => Some(LazyModule(new PerBankDRAM(params)(p)))
     case Some(params: AllBankDRAMParams) => Some(LazyModule(new AllBankDRAM(params)(p)))
+    case Some(params: MlpControlParams)  => Some(LazyModule(new MlpController(params)(p)))
     case None => None
   }
 
